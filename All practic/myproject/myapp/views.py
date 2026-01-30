d1173c4f0dfeb17e5dc70759e4ddc4ff7ff8209c
Mahmuda Khatun <mahmudak663@gmail.com>
2026-01-30 21:05:11 +0600

    auth

--- All practic/myproject/myapp/views.py
+++ All practic/myproject/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from myapp.models import*
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -10,19 +11,71 @@ def home(r):
     return render (r,'home.html')
 
 def signup(r):
+    if r.method=="POST":
+        username=r.POST.get('username')
+        Role=r.POST.get('Role')
+        password=r.POST.get('password')
+        email=r.POST.get('email')
+        confirm_password=r.POST.get('confirm_password')
+
+        user_exit=UserModel.objects.filter(username=username).exists()
+
+        if user_exit:
+            messages.warning(r,'user already exit')
+            return redirect('signup')
+
+        if confirm_password==password:
+              UserModel.objects.create_user(
+                username=username,
+                Role=Role,
+                password=confirm_password,
+                email=email,
+              )
+              return redirect ('signin')
+
     return render (r,'signup.html')
 
 def signin(r):
+    if r.method=="POST":
+        username=r.POST.get('username')
+        password=r.POST.get('password')
+
+        user=authenticate(r,username=username,password=password)
+
+        if user:
+            login (r,user)
+            messages.success(r,'successfully login')
+            return redirect('home')
+        else:
+            messages.warning(r,'invalid')
+            return redirect('signin')
     return render (r,'signin.html')
 
 def signout(r):
+    logout(r)
     return redirect ('signin')
 
 def chengepass(r):
+    current_user=r.user
+
+    if r.method=="POST":
+        current_password=r.POST.get('current_password')
+        new_password=r.POST.get('new_password')
+        confirm_password=r.POST.get('confirm_password')
+
+        if check_password(current_password,current_user.password):
+
+            if new_password==confirm_password:
+                current_user.set_password('new_password')
+                current_user.save()
+                update_session_auth_hash(r,current_user)
+                return redirect('home')
+
     return render (r,'chengepass.html')
 
 def dep_page(r):
-    return render (r,'department.html')
+    d_data=
+    return render (r,'dep_page.html')
 
 def dep_edit(r,id):
     return render (r,'dep_edit.html')
--- All practic/myproject/myproject/urls.py
+++ All practic/myproject/myproject/urls.py
@@ -4,7 +4,15 @@ from django.urls import path
 from myapp.views import*
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name='home'),
-    path('',signup,name='home'),
-    path('',home,name='home'),
+    path('home/',home,name='home'),
+    path('',signup,name='signup'),
+    path('signin/',signin,name='signin'),
+    path('signout/',signout,name='signout'),
+    path('chengepass/',chengepass,name='chengepass'),
+    path('dep_page/',dep_page,name='dep_page'),
+    path('dep_edit/<int:id>/',dep_edit,name='dep_edit'), 
+    path('dep_delete/<int:id>/',dep_delete,name='dep_delete'), 
+    path('studentpage/',studentpage,name='studentpage'), 
+    path('studentEdit/<int:id>/',studentEdit,name='studentEdit'), 
+    path('studentDelete/<int:id>/',studentDelete,name='studentDelete'), 
 ]
